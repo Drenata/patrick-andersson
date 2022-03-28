215dea2298764d4092f9f2a522ddf0b5076d5a15
Patrick Andersson <patrick.andersson@wiretronic.com>
2022-03-28 20:33:24 +0200

    extend screensize hook with callback and extra event

--- src/components/hooks/screensize.ts
+++ src/components/hooks/screensize.ts
@@ -1,20 +1,23 @@
 import { useState, useEffect } from "react";
 
-export default function useScreenSize() {
+export default function useScreenSize(onResize?: (width: number, heigth: number) => void) {
     const [width, setWidth] = useState(window.innerWidth);
     const [height, setHeight] = useState(window.innerHeight);
 
     useEffect(() => {
-        function onResize() {
+        function _onResize() {
             setWidth(window.innerWidth);
             setHeight(window.innerHeight);
+            if (onResize) onResize(window.innerWidth, window.innerHeight);
         }
 
-        window.addEventListener("resize", onResize);
-        window.addEventListener("load", onResize);
+        window.addEventListener("resize", _onResize);
+        window.addEventListener("load", _onResize);
+        window.addEventListener("orientationchange", _onResize);
         return function () {
-            window.removeEventListener("resize", onResize);
-            window.removeEventListener("load", onResize);
+            window.removeEventListener("resize", _onResize);
+            window.removeEventListener("load", _onResize);
+            window.removeEventListener("orientationchange", _onResize);
         };
     });
     return { height, width };
